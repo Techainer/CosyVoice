150f78e38371c2d4cfcc175bbe4edc727dff391a
Sam <dangvansam98@gmail.com>
2025-05-03 16:35:01 +0000

    update training config, script

--- examples/libritts/cosyvoice2/run_all_data.sh
+++ examples/libritts/cosyvoice2/run_all_data.sh
@@ -69,7 +69,7 @@ if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
   if [ $train_engine == 'deepspeed' ]; then
     echo "Notice deepspeed has its own optimizer config. Modify conf/ds_stage2.json if necessary"
   fi
-  # for model in llm flow; do
+  # for model in hifigan; do
   for model in llm; do
     echo "======================"
     echo "START TRAINING: $model"
@@ -79,6 +79,7 @@ if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
         --config conf/cosyvoice2.yaml \
         --train_data $output_raw_data_dir/train/parquet/data.list \
         --cv_data $output_raw_data_dir/valid/parquet/data.list \
+        --qwen_pretrain_path $pretrained_model_dir/CosyVoice-BlankEN \
         --model $model \
         --checkpoint $pretrained_model_dir/$model.pt \
         --model_dir `pwd`/exp/$exp_name/$model \
@@ -90,7 +91,21 @@ if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
   done
 fi
 
+# average model
+average_num=5
 if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
+  for model in llm flow hifigan; do
+    decode_checkpoint=`pwd`/exp/cosyvoice/$model/$train_engine/${model}.pt
+    echo "do model average and final checkpoint is $decode_checkpoint"
+    python cosyvoice/bin/average_model.py \
+      --dst_model $decode_checkpoint \
+      --src_path `pwd`/exp/cosyvoice/$model/$train_engine  \
+      --num ${average_num} \
+      --val_best
+  done
+fi
+
+if [ ${stage} -le 7 ] && [ ${stop_stage} -ge 7 ]; then
   echo "Export your model for inference speedup. Remember copy your llm or flow model to model_dir"
   python cosyvoice/bin/export_jit.py --model_dir $pretrained_model_dir
   python cosyvoice/bin/export_onnx.py --model_dir $pretrained_model_dir
